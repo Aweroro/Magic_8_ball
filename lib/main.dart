a23e8bf378e67dd85dab69cec5d45899bfce73b5
Aweroro Hadi <60360809+Aweroro@users.noreply.github.com>
2022-04-07 19:53:08 +0100

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.cyan.shade200,
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlue.shade800,
+          title: Text('Ask Me Anything'),
+        ),
+        body: Ball());
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+          print(ballNumber);
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
+  }
+}
